b47dbcb1acf5062e3b892c74432ea9c8d7494457
Antoine Brunet <contact@toinebru.net>
2017-11-14 12:47:19 +0100

    Ajout service checkstate

--- src/fyt_mae_node.cpp
+++ src/fyt_mae_node.cpp
@@ -3,6 +3,7 @@
 #include "cmg_msgs/Signal.h"
 #include "fyt_mae/fyt_mae.h"
 #include "fyt_mae/fyt_commons.h"
+#include "fyt_mae/CheckState.h"
 
 namespace fyt_mae {
 	class ROSMAE : public MAE {
@@ -10,6 +11,7 @@ namespace fyt_mae {
 			ros::NodeHandle n;
 			ros::Publisher state_pub;
 			ros::Subscriber signal_sub;
+			ros::ServiceServer state_srv;
 		public:
 			virtual void warn_ignore(state_t st, signal_t sig) override {
 				ROS_WARN("Ignoring signal %ud from state %ud", sig, st);
@@ -22,9 +24,15 @@ namespace fyt_mae {
 				}
 			}
 
+			bool check_state(fyt_mae::CheckState::Request &req, fyt_mae::CheckState::Response &res) {
+				res.state = current_state;
+				return true;
+			}
+
 			ROSMAE() : MAE(FYT_INIT_STATE), n("~") {
 				signal_sub = n.subscribe("signal", 1, &ROSMAE::sig_cb, this);
 				state_pub = n.advertise<cmg_msgs::State>("state", 1);
+				state_srv = n.advertiseService("check_state", &ROSMAE::check_state, this);
 				
 				add_transition(STATE_SAFE,	STATE_SAFE,	SIG_ALARM);
 				add_transition(STATE_READY,	STATE_SAFE,	SIG_ALARM);
